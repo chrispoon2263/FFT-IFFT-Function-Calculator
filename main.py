4864cf7a08ae6a0310b2e0e20e1cc880a99bd6d0
ChrisPoon <chris@ChrisPoons-MacBook-Pro.local>
2024-02-11 21:03:28 -0600

    updated files for flask

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about_me/')
+def about_me():
+    return render_template('about_me.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
